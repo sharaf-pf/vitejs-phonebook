af0995a1441339e137f0ebed3d2ff36c169cc39d
sharaf-pf <mohamed.abdallah.c@propertyfinder.ae>
2024-09-16 19:32:48 +0400

    changed baseUrl in phonebook.jsx

--- part2-exercises/src/services/phonebook.jsx
+++ part2-exercises/src/services/phonebook.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://vitejs-phonebook-server.onrender.com/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
